ba730d7749713d578cdd21e7bdea5895aac162f6
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-05 20:40:37 +0530

    First Commit

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -10,18 +10,41 @@ namespace UserRegex
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program");
-            Console.WriteLine("Enter your first name with first letter in capitals ");
-            string fname = Console.ReadLine();
-            string name_pattern = "^[A-Z]{1}[a-z]{2,}";
-            Regex regex = new Regex(name_pattern);
-            if (regex.IsMatch(fname))
+            string fname, lname;
+            string name_pattern;
+            while (true)
             {
-                Console.WriteLine("First Name is valid");
+                Console.WriteLine("Enter your first name with first letter in capitals ");
+                fname = Console.ReadLine();
+                name_pattern = @"^[A-Z]{1}[a-z]{2,}";
+                Regex regex = new Regex(name_pattern);
+                if (regex.IsMatch(fname))
+                {
+                    Console.WriteLine("Valid First Name");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid First Name");
+                }
             }
-            else
+            while (true)
             {
-                Console.WriteLine("First Name is Invalid");
+                Console.WriteLine("Enter your last name(only first letter in CAPS): ");
+                lname = Console.ReadLine();
+                name_pattern = @"^[A-Z]{1}[a-z]{2,}";
+                Regex regex = new Regex(name_pattern);
+                if (regex.IsMatch(lname))
+                {
+                    Console.WriteLine("Valid Last Name");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Last Name");
+                }
             }
+            Console.WriteLine("Your Entered Name is " + fname + " " + lname);
         }
     
     }
