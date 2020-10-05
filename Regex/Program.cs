d6ed5ec5362afe6a8fb15e0ffd5a03af52687bfc
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-05 19:19:19 +0530

    First Commit

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 
-namespace Regex
+namespace UserRegex
+
 {
     class Program
+    
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program");
-            
+            Console.WriteLine("Enter your first name with first letter in capitals ");
+            string fname = Console.ReadLine();
+            string name_regex = "^[A-Z]{1}[a-z]{2,}";
+            Regex regex = new Regex(name_regex);
+            if (regex.IsMatch(fname))
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name is Invalid");
+            }
         }
+    
     }
 }
