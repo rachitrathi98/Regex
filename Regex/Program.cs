0ab0220f8b89cc8dbc4a89945c6536dbe617211b
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-08 09:03:36 +0530

    First Commit

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -12,7 +12,7 @@ namespace UserRegex
             Console.WriteLine("Welcome to User Registration Program");
             Console.WriteLine("Enter your first name with first letter in capitals ");
             string fname = Console.ReadLine();
-            string name_pattern = "^[A-Z]{1}[a-z]{2,}";
+            string name_pattern = "^[A-Z]{1}[a-z]{2,}";//Name starts with Cap and has 3 minimum characters
             Regex regex = new Regex(name_pattern);
             if (regex.IsMatch(fname))
             {
